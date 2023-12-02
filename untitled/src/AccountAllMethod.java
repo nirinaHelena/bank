964a5fe3eb220ae8728e03a975646f66abe291bc
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2023-12-02 22:46:41 +0300

    Delete untitled/src/AccountAllMethod.java

--- untitled/src/AccountAllMethod.java
+++
@@ -1,6 +0,0 @@
-public interface AccountAllMethod {
-    public Double consult();
-    public  Double debit(Integer n);
-    public Double credit(Integer n);
-    public Double transaction(Integer n, Account receiver);
-}
