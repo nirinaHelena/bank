b82c94247ed46ac07df6ebeacb2c2d5b764c2055
nirinaHelena <hei.nirina.7@gmail.com>
2023-12-02 23:01:01 +0300

    add method calculInterest

--- untitled/src/bank/SavingsAccount.java
+++ untitled/src/bank/SavingsAccount.java
@@ -4,4 +4,10 @@ public class SavingsAccount extends BankAccount{
     public SavingsAccount(String accountId, Double balance) {
         super(accountId, balance);
     }
+    private Double interestRate=2.0;
+    public void calculInterest(){
+        double interest= getBalance()*(interestRate/100);
+        credit(interest);
+        System.out.println("intérêts calculés, nouveau solde: "+getBalance());
+    }
 }
