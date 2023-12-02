26d0336730de54e8e3f53ceb8b154741bb010a29
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2023-12-02 14:46:45 +0300

    Update MobileAccount.java

--- MobileAccount.java
+++ MobileAccount.java
@@ -2,4 +2,9 @@ public class MobileAccount extends Account {
         public MobileAccount(double initialBalance) {
             super(initialBalance);
         }
+        public void loan(Integer amount){
+            if (amount>0){
+                setBalance(getBalance()+amount);
+            }
+        }
 }
